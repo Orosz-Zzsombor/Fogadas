fbc63ae1d81d69a67b768f695a7f117f43b3fed9
orosz.zsombor.oliver <orosz.zsombor12@gmail.com>
2024-10-03 11:42:17 +0200

    Fogadas  0.1

---
+++ Fogadas/Event.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fogadas
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Category { get; set; }
+        public string Location { get; set; }
+    }
+    public class EventService
+    {
+        // Replace these values with your MySQL database details
+        private string connectionString = "Server=localhost;Database=FogadasDB;Uid=root;Pwd=;";
+
+        // Method to fetch all current events (events happening today or in the future)
+        public List<Event> GetCurrentEvents()
+        {
+            List<Event> events = new List<Event>();
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // SQL query to get all current events where the EventDate is today or in the future
+                    string query = "SELECT * FROM Events WHERE EventDate >= CURDATE()";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Event evt = new Event
+                                {
+                                    EventID = reader.GetInt32("EventID"),
+                                    EventName = reader.GetString("EventName"),
+                                    EventDate = reader.GetDateTime("EventDate"),
+                                    Category = reader.GetString("Category"),
+                                    Location = reader.GetString("Location")
+                                };
+
+                                events.Add(evt);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle or log the exception
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            return events;
+        }
+    }
+}
